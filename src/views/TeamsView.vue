52b09b169a0e7b4523e4669c967eae97cf13b0ee
Baptiste Marcel <baptiste.marcel@etu.uca.fr>
2023-03-13 16:07:54 +0100

    Ajout bouton modif nom et description d'une team

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -8,6 +8,10 @@
     <add-team-form @add-team="setTeams" @emptyError="displayError"/>
     <div v-for="team in allTeams">
         <team-card :id="team.id" :name="team.name" :description="team.description.length > 20 ? team.description.substring(0,20) + '...' : team.description"/>
+        <div>
+            <button @click="modifyTeamName(team.id)">Changer le nom</button>
+            <button @click="modifyTeamDescription(team.id)">Changer la description</button>
+        </div>
     </div>
 </template>
 
@@ -30,6 +34,38 @@
             displayError(error){
                 const divError = document.getElementById("divError");
                 divError.innerHTML = error;
+            },
+
+            modifyTeamName(id){
+                const team = this.allTeams.find(team => team.id === id);
+                const newName = prompt("Nouveau nom de l'équipe", team.name);
+                
+                if(newName === null){
+                    this.displayError("Le nom de l'équipe ne peut pas être vide");
+                }
+                else if(newName.length < 5){
+                    this.displayError("Le nom de l'équipe doit faire au moins 5 caractères");
+                }
+                else{
+                    this.displayError("");
+                    team.name = newName;
+                }
+            },
+
+            modifyTeamDescription(id){
+                const team = this.allTeams.find(team => team.id === id);
+                const newDescription = prompt("Nouvelle description de l'équipe", team.description);
+                
+                if(newDescription === null){
+                    this.displayError("La description de l'équipe ne peut pas être vide");
+                }
+                else if(newDescription.length < 20){
+                    this.displayError("La description de l'équipe doit faire au moins 20 caractères");
+                }
+                else{
+                    this.displayError("");
+                    team.description = newDescription;
+                }
             }
         }
     };
